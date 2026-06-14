92b4ec659d22cacb40d8cdbc7d3a1c6f1890ebb1
shanalaufer <shanalaufer@gmail.com>
2026-06-14 03:08:01 +0300

    more UI fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,23 @@
+// Polyfills for iOS < 15.4 (needed by pdfjs-dist v6)
+if (!Array.prototype.at) {
+  Object.defineProperty(Array.prototype, 'at', {
+    value(n) { n = Math.trunc(n) || 0; if (n < 0) n += this.length; return (n < 0 || n >= this.length) ? undefined : this[n] },
+    writable: true, configurable: true,
+  })
+}
+if (!String.prototype.at) {
+  Object.defineProperty(String.prototype, 'at', {
+    value(n) { n = Math.trunc(n) || 0; if (n < 0) n += this.length; return (n < 0 || n >= this.length) ? undefined : this[n] },
+    writable: true, configurable: true,
+  })
+}
+if (!Object.hasOwn) {
+  Object.hasOwn = (obj, key) => Object.prototype.hasOwnProperty.call(obj, key)
+}
+if (typeof structuredClone === 'undefined') {
+  globalThis.structuredClone = obj => JSON.parse(JSON.stringify(obj))
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
